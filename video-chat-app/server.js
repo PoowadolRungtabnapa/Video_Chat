15601f03b751886898ae61cc53201dd379c08cd9
PoowadolRungtabnapa <s6206022610033@email.kmutnb.ac.th>
2021-09-09 16:21:12 +0700

    .

--- video-chat-app/server.js
+++ video-chat-app/server.js
@@ -1,11 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require("http").Server(app);
-const { v4: uuidv4 } = require("uuid");
+const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+app.set('view engine', 'ejs');
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+debug: true,
+});
+app.use('/peerjs', peerServer);
 app.use(express.static('public'));
-app.set('view engine', 'ejs')
-
-app.get("/", (req, res) => {
-    res.render("room");
+app.get('/', (req, res) => {
+res.redirect(`/${uuidv4()}`);
+});
+app.get('/:room', (req, res) => {
+res.render('room', { roomId: req.param.room });
+});
+io.on('connection', (socket) => {
+socket.on('join-room', (roomId, userId) => {
+socket.join(roomId);
+socket.to(roomId).broadcast.emit('user-connected', userId);
+});
 });
 server.listen(3030);
