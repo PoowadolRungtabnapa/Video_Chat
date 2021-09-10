06deb6504e264ff0ff08ad8cf2c8dc708112ec44
PoowadolRungtabnapa <s6206022610033@email.kmutnb.ac.th>
2021-09-10 17:05:43 +0700

    .

---
+++ Test3/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, Response
+from camera import Camera
+from time import time
+
+class Camera(object):
+    def __init__(self):
+        self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+
+    def get_frame(self):
+        return self.frames[int(time()) % 3]
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="192.168.2.108")
